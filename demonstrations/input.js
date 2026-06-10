f70f94dc64497083686990cdb0c21edb12d04f86
Emerson Shaw <123201331+eshaw463@users.noreply.github.com>
2026-06-10 14:04:10 -0500

    Create input.js

---
+++ demonstrations/input.js
@@ -0,0 +1,33 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const input = (question) => {
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      resolve(answer);
+    });
+  });
+};
+
+const main = async () => {
+  // Get user input using await
+  const name = await input('');
+
+  console.log(`Hello, ${name}!`);
+
+  const inty = await input('');
+
+  console.log(parseInt(inty) + 4);
+
+  rl.close();
+};
+
+// Call the main async function
+main();
+
+// quite a bit of work to get input to work :(
+// this is why java/python may be better for this specifically but that isn't totally necessary
